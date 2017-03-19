939e5acc15a6b3e43cc27ca12a37881dbd510c0d
wonpera <wonpera_562@163.com>
2017-03-19 22:28:12 +0800

    修改建议新增保存乱码问题

--- website-web/src/main/java/com/dky/website/web/controller/front/FrontSuggestController.java
+++ website-web/src/main/java/com/dky/website/web/controller/front/FrontSuggestController.java
@@ -8,7 +8,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * Created by hang on 2017/2/27.
@@ -23,10 +26,21 @@ public class FrontSuggestController {
 
 
     @RequestMapping("addSuggest")
-    public ReturnT getProductWithImg(@Valid AddFrontSuggestParam param,BindingResult bindingResult){
+    public ReturnT getProductWithImg(@Valid AddFrontSuggestParam param,BindingResult bindingResult,HttpServletRequest request){
         if(bindingResult != null && bindingResult.hasErrors()){
             String errorMsg = bindingResult.getFieldError().getDefaultMessage();
             return new ReturnT().failureData(errorMsg);
+        }
+        try {
+
+            String name = URLDecoder.decode(param.getName(), "UTF-8"); // new String(request.getParameter("name").getBytes(), "UTF-8");
+            String title =  URLDecoder.decode(param.getTitle(), "UTF-8");//new String(request.getParameter("title").getBytes(), "UTF-8");
+            String content =  URLDecoder.decode(param.getContent(), "UTF-8");//new String(request.getParameter("content").getBytes(), "UTF-8");
+            param.setName(name);
+            param.setTitle(title);
+            param.setContent(content);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
         return suggestService.addSuggest(param);
     }
